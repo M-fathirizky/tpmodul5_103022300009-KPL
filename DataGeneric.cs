0354ce40159031dd53489bed153505914be98184
M-fathirizky <fathirrizky636@gmail.com>
2025-03-16 13:03:58 +0700

    “menambahkan class DataGeneric

---
+++ DataGeneric.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103022300009
+{
+    class DataGeneric<T>
+    {
+        // Properti data dengan tipe data generik
+        private T data;
+
+        // Konstruktor
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        // Method PrintData 
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
+}
